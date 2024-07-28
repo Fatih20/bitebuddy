2c80f9078eff9ff2eeca5af512ef1680070e9c93
akbarmridho <akbarmaulanaridho1@gmail.com>
2024-07-28 01:07:24 +0000

    additional dataset

---
+++ dataset/import-new-cleaned.ts
@@ -0,0 +1,63 @@
+import weaviate, { DataObject, dataType, generateUuid5 } from "weaviate-client";
+import fs from "fs/promises";
+import { ActuallyFinal } from "./type.ts";
+
+const COLLECTION_NAME = "Menu";
+const RESTAURANT_COLLECTION = "Restaurant";
+
+(async function main() {
+  const client = await weaviate.connectToLocal();
+
+  const raw = await fs.readFile("additonal-cleaned.json", {
+    encoding: "utf-8",
+  });
+
+  const parsed = JSON.parse(raw) as ActuallyFinal[];
+
+  const restaurantSet = new Set<string>();
+
+  parsed.forEach((each) => {
+    restaurantSet.add(each.restaurantName);
+  });
+
+  const restaurantCollection = client.collections.get(RESTAURANT_COLLECTION);
+
+  const restaurantData = [...restaurantSet.values()].map((each) => {
+    return {
+      properties: {
+        name: each,
+      },
+      id: generateUuid5(RESTAURANT_COLLECTION, each),
+    };
+  }) as unknown as DataObject<undefined>[];
+
+  await restaurantCollection.data.insertMany(restaurantData);
+
+  const convertedData = parsed.map(({ restaurantName, ...each }) => {
+    return {
+      properties: each,
+      references: {
+        hasRestaurant: generateUuid5(RESTAURANT_COLLECTION, restaurantName),
+      },
+    };
+  }) as unknown as DataObject<undefined>[];
+
+  const myCollection = client.collections.get(COLLECTION_NAME);
+
+  const length = convertedData.length;
+  const batchSize = 50;
+  const batchCount = Math.ceil(length / batchSize);
+
+  let imported = 0;
+
+  for (let i = 0; i < batchCount; i++) {
+    const slice = convertedData.slice(
+      i * batchSize,
+      Math.min((i + 1) * batchSize, length)
+    );
+    await myCollection.data.insertMany(slice);
+    imported += slice.length;
+
+    console.log(`Data imported ${imported}`);
+  }
+})();
